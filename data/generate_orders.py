f1a1c4c2255f7d99ea6116e946bc0ebe2b60d985
KiranWells <kiranwells1008@gmail.com>
2023-02-27 08:09:55 -0600

    Create initial python script for making order data

---
+++ data/generate_orders.py
@@ -0,0 +1,146 @@
+"""
+A simple python script to generate
+orders data
+"""
+
+import random
+from datetime import datetime
+from typing import Optional
+
+# Schema:
+
+CREATE_COMMAND = """
+CREATE TABLE order_history (
+    id bigserial PRIMARY KEY,
+    order_id int NOT NULL,
+    order_date timestamp NOT NULL,
+    item_id bigint NOT NULL FOREIGN KEY REFERENCES menu_items (item_id),
+    price numeric(10,2) NOT NULL,
+    payment_method varchar(50) NOT NULL,
+    quantity int NOT NULL
+);
+"""
+
+ITEM_COUNT = 100
+
+
+class Order:
+    """
+    A simple class to represent an order
+    """
+
+    order_id: int
+    order_date: datetime  # timestamp
+    item_ids: list[int]
+    price: float
+    payment_method: Optional[str]
+    quantity: list[int]
+
+    def __init__(self, order_id: int, date: datetime):
+        self.order_id = order_id
+        self.order_date = date
+        self.item_ids = []
+        self.payment_method = random.choice(
+            ["cash", "card", "dining_dollars", "retail_swipe", "meal_plan_both"]
+        )
+        self.quantity = []
+        self.price = self.get_price()
+
+    def __str__(self):
+        out = ""
+        price = f"{round(self.price, 2)}"
+        for item_id, quantity in zip(self.item_ids, self.quantity):
+            out += f"{self.order_id}, {self.order_date}, {item_id}, \
+{price}, {self.payment_method}, {quantity}\n"
+        return out
+
+    def get_price(self):
+        """Returns the price of an order, and sets the item_ids and quantity"""
+        price = 0
+        # family meals
+        if random.randint(1, 100) < 1:
+            price += 31.50
+            self.item_ids = random.sample(range(ITEM_COUNT), 4)
+            # drinks
+            for _ in range(random.randint(1, 3)):
+                price += random.randint(17, 21) * 0.10
+        else:
+            # appetizers
+            if random.randint(1, 100) < 3:
+                price += random.randint(1, 3) * 1.90
+
+            # entrees
+            if random.randint(1, 100) < 95:
+                if random.randint(1, 100) < 10:  # kids meal
+                    price += 5.00
+                else:
+                    # normal entrees
+                    price += 6.00
+                    entree_price = 1.50 if random.randint(1, 100) < 10 else 2.75
+                    for _ in range(random.randint(1, 3)):
+                        price += entree_price
+            # a-la-carte
+            else:
+                for _ in range(random.randint(0, 3)):
+                    price += random.choice([3.70, 4.95, 7.00, 9.50, 9.70, 13.45])
+
+            # drinks
+            if random.randint(1, 100) < 50:
+                price += random.randint(17, 21) * 0.10
+
+        return price
+
+
+def get_popularity(month: int, day: int, hour: int) -> int:
+    """Returns the maximum number of orders for a given hour based on
+    the behavior of a college clientele"""
+    game_days = [(9, 1), (10, 5)]
+
+    popularity = 60  # base popularity
+
+    if (month, day) in game_days:
+        popularity *= 5
+
+    if hour in (12, 13, 18, 19):  # lunch and dinner
+        popularity *= 2
+
+    if month in (6, 7, 8):  # summer
+        popularity /= 3
+
+    return int(popularity)
+
+
+def create_orders():
+    """Create a list of orders"""
+    order_id_next = 0
+    for month in range(1, 13):
+        for day in range(1, 31):
+            try:
+                for hour in range(10, 20):  # 10am - 8pm
+                    popularity = get_popularity(month, day, hour)
+                    for _ in range(0, random.randint(0, popularity)):
+                        minute = random.randint(0, 59)
+                        second = random.randint(0, 59)
+                        date = datetime(2020, month, day, hour, minute, second)
+                        yield Order(order_id_next, date)
+                        order_id_next = order_id_next + 1
+            except ValueError:
+                # Skip invalid dates
+                continue
+
+
+def main():
+    """Entry point"""
+    print(CREATE_COMMAND)
+    total_price = 0
+    with open("order_history.csv", "w", encoding="utf-8") as file:
+        file.write("order_id, order_date, item_id, price, payment_method, quantity\n")
+        for order in create_orders():
+            total_price += order.price
+            file.write(str(order))
+
+    print(f"Total price: {round(total_price, 2)}")
+
+
+if __name__ == "__main__":
+    main()
