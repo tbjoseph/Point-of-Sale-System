9ceb4d22143adabeea7f6cf55992d551947cd70b
Timothy Binu Joseph <90801714+tbjoseph@users.noreply.github.com>
2023-02-27 15:46:25 -0600

    Create shipment.py
    List of ingredients

---
+++ shipment.py
@@ -0,0 +1,11 @@
+materials = [ 
+    #meats and grains
+    'chicken', 'beef', 'shrimp', 'tofu', 'rice', 'chow mein noodles',
+    #vegetables
+    'broccoli', 'green beans', 'carrots', 'cabbage', 'bell peppers', 'mushrooms',
+    'black pepper' , 'chili peppers',
+    #drinks
+    'water', 'soda pack',
+    #sauce ingredients
+    'soy sauce', 'vinegar', 'sugar', 'orange juice', 'garlic', 'ginger', 'pineapple juice'
+    ]
