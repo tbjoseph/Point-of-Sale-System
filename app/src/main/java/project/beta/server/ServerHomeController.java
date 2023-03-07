c7d7fcc8b056838ec27a90238d73b0ccb3c9e86c
mgimlin <94798985+mgimlin@users.noreply.github.com>
2023-03-07 14:12:24 -0600

    Added labels on the server home screen. Planning code structure.

--- app/src/main/java/project/beta/server/ServerHomeController.java
+++ app/src/main/java/project/beta/server/ServerHomeController.java
@@ -4,11 +4,40 @@ import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
 
 public class ServerHomeController {
-    public void buttonPress(ActionEvent event) {
-        Button sourceButton = (Button) event.getSource();
-        String buttonName = sourceButton.getText();
+    /**
+     * Add a side item to the current order.
+     * 
+     * @param event Used to get the button name of the side item.
+     */
+    public void addSide(ActionEvent event) {
+        Button sideButton = (Button) event.getSource();
+        String sideName = sideButton.getText();
 
-        System.out.println(buttonName + " button pressed");
+        // TODO:
+        // add the side to the order depending on the mode
+        // a la carte, bowl, plate, etc.
+        System.out.println(sideName + " button pressed");
+    }
+
+    /**
+     * Add an entree to the current order.
+     * 
+     * @param event Used to get the button name of the entree.
+     */
+    public void addEntree(ActionEvent event) {
+        Button entreeButton = (Button) event.getSource();
+        String entreeName = entreeButton.getText();
+
+        // TODO:
+        // add the entree to the order depending on the mode
+        // a la carte, bowl, plate, etc.
+        System.out.println(entreeName + " button pressed");
+    }
+
+    public void nextScreen(ActionEvent event) {
+        // TODO:
+        // change the scene to the drinks and appetizer screen
+        // pass the current order to the next screen
     }
 
     public void initialize() {
